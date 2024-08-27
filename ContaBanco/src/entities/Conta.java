d1e19d49e8377aa5c61806abfb3a9f70abca0a19
Tchauperas <marcos.vhynycyus@gmail.com>
2024-08-27 14:51:17 -0300

    inicial commit

---
+++ ContaBanco/src/entities/Conta.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Conta {
+	private int numero;
+	private String agencia;
+	private String nomeCliente;
+	private double saldo;
+	
+	public Conta() {}
+
+	public Conta(int numero, String agencia, String nomeCliente, double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.nomeCliente = nomeCliente;
+		this.saldo = saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+}
